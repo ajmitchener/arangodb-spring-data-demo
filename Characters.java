c3d418068443bf26d1f3598673e7dabf0b068d79
ajmitchener <ajmitchener@gmail.com>
2024-01-03 12:05:31 -0500

    Create Characters.java

---
+++ Characters.java
@@ -0,0 +1,46 @@
+package com.arangodb.spring.demo.entity;
+ 
+import com.arangodb.springframework.annotation.Document;
+import org.springframework.data.annotation.Id;
+ 
+@Document("characters")
+public class Character {
+ 
+    @Id // db document field: _key
+    private String id;
+ 
+    @ArangoId // db document field: _id
+    private String arangoId;
+ 
+    private String name;
+    private String surname;
+    private boolean alive;
+    private Integer age;
+ 
+    public Character() {
+        super();
+    }
+ 
+    public Character(final String name, final String surname, final boolean alive) {
+        super();
+        this.name = name;
+        this.surname = surname;
+        this.alive = alive;
+    }
+ 
+    public Character(final String name, final String surname, final boolean alive, final Integer age) {
+        super();
+        this.name = name;
+        this.surname = surname;
+        this.alive = alive;
+        this.age = age;
+    }
+ 
+    // getter & setter
+ 
+    @Override
+    public String toString() {
+        return "Character [id=" + id + ", name=" + name + ", surname=" + surname + ", alive=" + alive + ", age=" + age + "]";
+    }
+ 
+}
